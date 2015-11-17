301e6954d87e12060d4e08c498c5ad7de2928d6c
wilomgfx <william@hugocantin.info>
2015-11-17 14:54:03 -0500

    added the righ db name to context

--- ProjetSessionWebServ2/ProjetSessionWebServ2/Models/IdentityModels.cs
+++ ProjetSessionWebServ2/ProjetSessionWebServ2/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace ProjetSessionWebServ2.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ProjSessionWebServ2", throwIfV1Schema: false)
         {
         }
 
