341ec9590057aa3d9f7758f50c523beb02a65c67
blocdeglace19952 <1250824@LLBD0604INFO12.laboratoire.collegeem.qc.ca>
2015-11-24 14:07:09 -0500

    Ajout modèle conférence

---
+++ ProjetSessionWebServ2/ProjetSessionWebServ2/Models/Conference.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetSessionWebServ2.Models
+{
+    public class Conference : Evenement
+    {
+        public TypeConference TypeConference { get; set; }
+    }
+}
