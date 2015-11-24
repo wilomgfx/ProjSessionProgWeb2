a5a7712108ca9ebbc76b8e4e79d4c007541703af
blocdeglace19952 <1250824@LLBD0604INFO12.laboratoire.collegeem.qc.ca>
2015-11-24 14:20:31 -0500

    Ajout de ConferenceRepository

---
+++ ProjetSessionWebServ2/ProjetSessionWebServ2/DAL/ConferenceRepository.cs
@@ -0,0 +1,25 @@
+﻿using GestionPhotoImmobilier.DAL;
+using ProjetSessionWebServ2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetSessionWebServ2.DAL
+{
+    public class ConferenceRepository : GenericRepository<Conference>
+    {
+        public ConferenceRepository(ApplicationDbContext context) : base(context) { }
+        public Conference ObtenirConferenceParID(int? id)
+            {
+                return GetByID(id);
+            }
+        public IEnumerable<Conference> ObtenirConference()
+            {
+                return Get();
+            }
+        public void InsertForfait(Conference Conference) { Insert(Conference); }
+        public void DeleteForfait(Conference Conference) { Delete(Conference); }
+        public void UpdateForfait(Conference Conference) { Update(Conference); }
+    }
+}
