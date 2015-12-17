2751311d3e68bdcfc77a4d795e37a160fabf0220
blocdeglace19952 <jonathanplamondon2@gmail.com>
2015-12-17 16:15:08 -0500

    Traduction evenement

--- ProjetSessionWebServ2/ProjetSessionWebServ2/Models/Evenement.cs
+++ ProjetSessionWebServ2/ProjetSessionWebServ2/Models/Evenement.cs
@@ -11,6 +11,7 @@ namespace ProjetSessionWebServ2.Models
         public int Id { get; set; }
         [Display(Name = "Name", ResourceType = typeof(GlobalRessources.EvenementRes))]
         public string Nom {get;set;}
+         [Display(Name = "EventType", ResourceType = typeof(GlobalRessources.EvenementRes))]
         public enum TypeEvent
         {
            TypeTournoi,
@@ -21,11 +22,11 @@ namespace ProjetSessionWebServ2.Models
         };
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
-
+        [Display(Name = "EventType", ResourceType = typeof(GlobalRessources.EvenementRes))]
         public TypeEvent TypeEvenement { get; set; }
-
+        [Display(Name = "PlageHoraire", ResourceType = typeof(GlobalRessources.EvenementRes))]
         public List<PlageHoraire> PlageHoraires { get; set; }
-
+        [Display(Name = "User", ResourceType = typeof(GlobalRessources.EvenementRes))]
         public List<ApplicationUser> Users { get; set; }
 
         [Display(Name = "Room", ResourceType = typeof(GlobalRessources.EvenementRes))]
@@ -33,7 +34,7 @@ namespace ProjetSessionWebServ2.Models
 
         [Display(Name = "Active", ResourceType = typeof(GlobalRessources.EvenementRes))]
         public bool Actif { get; set; }
-
+         [Display(Name = "Congres", ResourceType = typeof(GlobalRessources.EvenementRes))]
         public virtual Congres Congres { get; set; }
 
         }
