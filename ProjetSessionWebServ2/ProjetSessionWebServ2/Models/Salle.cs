d6de530c24176cb425b167fa70640035f23f3601
blocdeglace19952 <jonathanplamondon2@gmail.com>
2015-12-17 15:55:12 -0500

    traduction salle/section

--- ProjetSessionWebServ2/ProjetSessionWebServ2/Models/Salle.cs
+++ ProjetSessionWebServ2/ProjetSessionWebServ2/Models/Salle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,13 @@ namespace ProjetSessionWebServ2.Models
     public class Salle
     {
         public int Id { get; set; }
-
+        [Display(Name = "NoSalle", ResourceType = typeof(GlobalRessources.SalleSectionRes))]
         public string NoSalle { get; set; }
-
+        [Display(Name = "Sections", ResourceType = typeof(GlobalRessources.SalleSectionRes))]
         public virtual List<Section> Sections { get; set; }
 
         public virtual Dimension Dimension { get; set; }
+        [Display(Name = "Taille", ResourceType = typeof(GlobalRessources.SalleSectionRes))]
         public Taille TailleSalle { get; set;}
     }
 }
--- ProjetSessionWebServ2/ProjetSessionWebServ2/Models/Section.cs
+++ ProjetSessionWebServ2/ProjetSessionWebServ2/Models/Section.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,14 @@ namespace ProjetSessionWebServ2.Models
     public class Section
     {
         public int Id { get; set; }
+        [Display(Name = "Nom", ResourceType = typeof(GlobalRessources.SalleSectionRes))]
         public string Nom { get; set; }
+        [Display(Name = "Taille", ResourceType = typeof(GlobalRessources.SalleSectionRes))]
         public Taille TailleSection { get; set; }
+        [Display(Name = "Salle", ResourceType = typeof(GlobalRessources.SalleSectionRes))]
         public Salle Salle { get; set; }
         public Nullable<int> EvenementId { get; set; }
+        [Display(Name = "Evenement", ResourceType = typeof(GlobalRessources.SalleSectionRes))]
         public Evenement Evenement { get; set; }
         public Dimension Dimension { get; set; }
         //public virtual Dimension Sections { get; set; } <--- this should not exists, it fked up the db :(
